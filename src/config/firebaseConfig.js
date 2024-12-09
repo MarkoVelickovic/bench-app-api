4b41d22ef2a79732764be1a5584b58096c09cbbe
vekimijovic <veljko.mijovic@hypercubesoft.com>
2024-12-09 17:39:22 +0100

    Test without env

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 // Initialize Firebase Admin SDK with service account key
 admin.initializeApp({
-  credential: admin.credential.cert(require(process.env.FIREBASE_CONFIG_PATH)),
+  credential: admin.credential.cert("bench-app-dd51e-firebase-adminsdk-6ek2i-1ac5b17c93.json"),
   storageBucket: "bench-app-dd51e.firebasestorage.app", // Replace with your Firebase Storage bucket name
   projectId: "bench-app-dd51e"
 });
